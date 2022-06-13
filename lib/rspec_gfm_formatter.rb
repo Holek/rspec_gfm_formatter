b617eb077f3ca5ba908620f7d8d4d48aeeaea081
Michał Połtyn <michal@poltyn.com>
2022-06-13 14:32:08 +0200

    Fix alias

--- lib/rspec_gfm_formatter.rb
+++ lib/rspec_gfm_formatter.rb
@@ -162,4 +162,4 @@ class RspecGfmFormatter < ::RSpec::Core::Formatters::BaseTextFormatter
 end
 
 # alias for convenience
-RSpecGfmFormatter = RSpecGfmFormatter
+RSpecGfmFormatter = RspecGfmFormatter
